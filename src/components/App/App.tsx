ee65058a6a7d902740099d6bcd1359204718c5c7
BobrikAU <kirbob0@yandex.ru>
2022-06-27 11:11:42 +0300

    featch: make a foundation

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1 style={{fontFamily: 'Iceland', fontSize: 30, color: '#fff', fontWeight: 400}}>kjvnksnlfksj</h1>
     </div>
   );
 }
