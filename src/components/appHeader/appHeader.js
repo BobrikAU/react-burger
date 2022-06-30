ca9e6b2de85f65d50935f25c70b80ef87863fbed
BobrikAU <kirbob0@yandex.ru>
2022-06-30 22:30:29 +0300

    bagfix: доработано наведение на ссылки в шапке

--- src/components/appHeader/appHeader.js
+++ src/components/appHeader/appHeader.js
@@ -4,34 +4,61 @@ import { Logo, BurgerIcon, ListIcon, ProfileIcon } from '@ya.praktikum/react-dev
 import LinkHeader from '../linkHeader/linkHeader';
 import PropTypes from 'prop-types';
 
-function AppHeader(props) {
-  return (
-    <header className={styles.header}>
-      <nav className={styles.navigation}>
-        <LinkHeader 
-          textColor={props.active === 'constructor' ? 'active' : 'inactive'}
-          icon={<BurgerIcon type={props.active === 'constructor' ? "primary" : "secondary"}/>}
-          >
-          Конструктор
-        </LinkHeader>
-        <LinkHeader 
-          textColor={props.active === 'orders' ? 'active' : 'inactive'}
-          icon={<ListIcon type={props.active === 'orders' ? "primary" : "secondary"}/>}
-          >
-          Лента заказов
-        </LinkHeader>
-      </nav>
-      <Logo />
-      <nav className={styles.accountLink}>
-        <LinkHeader 
-          textColor={props.active === 'account' ? 'active' : 'inactive'}
-          icon={<ProfileIcon type={props.active === 'account' ? "primary" : "secondary"}/>}
-          >
-          Личный кабинет
-        </LinkHeader>
-      </nav>
-    </header>
-  );
+class AppHeader extends React.Component {
+
+  constructor (props) {
+    super (props);
+  }
+
+  state = {
+    isHover: {
+      'Конструктор': false,
+      'Лента заказов': false,
+      'Личный кабинет': false
+    }
+  }
+
+  toggleIsHover = (newState) => {
+    this.setState(() => ({
+      isHover: {...newState}
+    }))
+  }
+
+  render() {
+    return (
+      <header className={styles.header}>
+        <nav className={styles.navigation}>
+          <LinkHeader 
+            textColor={this.props.active === 'constructor' ? 'active' : 'inactive'}
+            icon={<BurgerIcon type={this.props.active === 'constructor' || this.state.isHover['Конструктор'] ? "primary" : "secondary"}/>}
+            state={this.state.isHover}
+            changeIsHover={this.toggleIsHover}
+            >
+            Конструктор
+          </LinkHeader>
+          <LinkHeader 
+            textColor={this.props.active === 'orders' ? 'active' : 'inactive'}
+            icon={<ListIcon type={this.props.active === 'orders' || this.state.isHover['Лента заказов'] ? "primary" : "secondary"}/>} 
+            state={this.state.isHover}
+            changeIsHover={this.toggleIsHover}
+            >
+            Лента заказов
+          </LinkHeader>
+        </nav>
+        <Logo />
+        <nav className={styles.accountLink}>
+          <LinkHeader 
+            textColor={this.props.active === 'account' ? 'active' : 'inactive'}
+            icon={<ProfileIcon type={this.props.active === 'account' || this.state.isHover['Личный кабинет'] ? "primary" : "secondary"}/>}
+            state={this.state.isHover}
+            changeIsHover={this.toggleIsHover}
+            >
+            Личный кабинет
+          </LinkHeader>
+        </nav>
+      </header>
+    );
+  }
 }
 
 export default AppHeader;
