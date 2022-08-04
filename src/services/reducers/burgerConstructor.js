6ab27ee4a498bd1bff3b09e0424ba2408fe9a608
BobrikAU <kirbob0@yandex.ru>
2022-08-04 10:51:27 +0300

    feature: make initial storage

---
+++ src/services/reducers/burgerConstructor.js
@@ -0,0 +1,10 @@
+const initialState = {
+  bun: '',
+  others: [],
+}
+export const burgerConstructorReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
---
+++ src/services/reducers/orderDetails.js
@@ -0,0 +1,6 @@
+export const orderDetailsReducer = (state = {}, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
